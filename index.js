5bf94ed52df3e706d258773a228e057ac12193cb
VimmySabu <vimmysabu1998@gmail.com>
2022-02-11 09:02:21 +0530

    new

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(express.static('./build/'));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname +
     /build/index.html));
     });
