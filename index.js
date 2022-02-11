d7a0b96ba83b837bd9445dfdcf01ba7ea0c78f76
VimmySabu <vimmysabu1998@gmail.com>
2022-02-11 09:18:25 +0530

    new

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.static('./build'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname +/build/index.html));
+    res.sendFile(path.join(__dirname +'/build/index.html'));
     });
 
 app.post('/api/login',(req,res)=>{
