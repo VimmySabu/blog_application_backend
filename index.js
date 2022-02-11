300ea7ab67ab87f8eb24cab0aaaaf32877761e55
VimmySabu <vimmysabu1998@gmail.com>
2022-02-11 09:12:06 +0530

    new

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const app=express()
 app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
-app.use(express.static('./build/'));
+app.use(express.static('./build'));
 
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname +/build/index.html));
     });
 
@@ -105,6 +105,6 @@ app.post('/api/article/:name/comments',(req,res)=>{
         })
 })
 
-const port=process.env.PORT|| 5000
+const port=process.env.PORT || 5000
 
 app.listen(port,()=>console.log('Server on 5000'))
